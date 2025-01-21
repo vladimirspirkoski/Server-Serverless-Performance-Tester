c410213bc138e0720f5637a5a21ed802f556d22e
vladimirspirkoski <vladimir.spirkoski@outlook.com>
2025-01-21 22:14:54 +0100

    Update v1.3
    Revised memory test
    Each thread allocates its own memory instead of preallocating it before creating the threads

--- MemoryTest.py
+++ MemoryTest.py
@@ -1,23 +1,31 @@
 import time
 import numpy as np
+import gc
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def memory_test_task(num_elements, test_type, memory_array=None):
-    
+def memory_test_task(num_elements, test_type):
+
     if test_type == 'write':
+        start_time = time.time()
         # Napolni ja memorijata so random elementi
-        memory_array = np.random.randint(
-            0, 256, size=num_elements, dtype=np.uint8)
+        memory_array = np.full(num_elements, 42, dtype=np.uint8)
+        end_time = time.time()
         del memory_array  # Oslobodi memorija
+        gc.collect()
 
     elif test_type == 'read':
+        memory_array = np.full(num_elements, 42, dtype=np.uint8)
+        start_time = time.time()
         # Citaj so copy funkcija
         read_copy = memory_array.copy()
+        end_time = time.time()
         del memory_array
         del read_copy
+        gc.collect()
 
-    return
+    return end_time - start_time
+
 
 def memory_test(n_kb, test_type, threshold):
 
@@ -26,40 +34,18 @@ def memory_test(n_kb, test_type, threshold):
 
     output = ""
 
-    if (test_type == 'write'):
-        for i in range(1, threshold + 1):
-            start_time = time.time()
+    for i in range(1, threshold + 1):
+        task_durations = []
 
-            # modificiran kod od superfastpython.com
-            with ThreadPoolExecutor(max_workers=i) as executor:
-                futures = [executor.submit(
-                    memory_test_task, num_elements, test_type) for _ in range(i)]
-                for future in as_completed(futures):
-                    future.result()
+        # modificiran kod od superfastpython.com
+        with ThreadPoolExecutor(max_workers=i) as executor:
+            futures = [executor.submit(
+                memory_test_task, num_elements, test_type) for _ in range(i)]
+            for future in as_completed(futures):
+                task_durations.append(future.result())
         # presmetaj go vremeto i vrati go vo output
-            total_time = time.time() - start_time
-            output += f"Total time for {i} concurrent memory test(s): {
-                total_time:.2f} seconds<br>"
-        
-    elif (test_type == 'read'):
-        for i in range(1, threshold + 1):
-            #dinamicki kreiraj nizi
-            memory_arrays = [
-                np.random.randint(0, 256, size=num_elements, dtype=np.uint8) for _ in range(i)
-            ]
-            
-            start_time = time.time()
-            # modificiran kod od superfastpython.com
-            with ThreadPoolExecutor(max_workers=i) as executor:
-                futures=[]
-                futures = [
-                    executor.submit(memory_test_task, num_elements, test_type, memory_arrays[j]) for j in range(i)
-                ]
-                for future in as_completed(futures):
-                    future.result()
-        # presmetaj go vremeto i vrati go vo output
-            total_time = time.time() - start_time
-            output += f"Total time for {i} concurrent memory test(s): {
-                total_time:.2f} seconds<br>"
+        total_time = max(task_durations)
+        output += f"Total time for {i} concurrent memory test(s): {
+            total_time:.3f} seconds<br>"
 
     return output
